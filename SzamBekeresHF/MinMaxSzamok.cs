92921d62f94ee3ead2ef3742fe737f1a240abfd6
Máté <forthealli08@gmail.com>
2018-10-07 10:13:58 +0200

    1.feladat

---
+++ SzamBekeresHF/MinMaxSzamok.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzamBekeresHF
+{
+    class MinMaxSzamok
+    {
+        public static int MaxSzam(int[] tomb)
+        {
+            int max = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (max < tomb[i])
+                {
+                    max = tomb[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
--- SzamBekeresHF/Program.cs
+++ SzamBekeresHF/Program.cs
@@ -10,6 +10,24 @@ namespace SzamBekeresHF
     {
         static void Main(string[] args)
         {
+            int db = 0;
+            int szam;
+            List<int> szamLista = new List<int>();
+            int[] szamTomb = new int[10];
+
+            Console.WriteLine("Kérem adjon meg 10db számot!");
+            do
+            {
+                Console.Write((db + 1) + ". szám : ");
+                szam = Convert.ToInt32(Console.ReadLine());
+                szamTomb[db] = szam;
+                szamLista.Add(szam);
+                db++;
+            } while (db < 10);
+
+            Console.WriteLine("\n(Tömb) Max : " + MinMaxSzamok.MaxSzam(szamTomb));
+            Console.WriteLine("\n(Lista) Max : " + szamLista.Max());
+            Console.ReadKey();
         }
     }
 }
