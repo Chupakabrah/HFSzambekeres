325dc43ce933560a3584f34cbddd69e57232d52d
Máté <forthealli08@gmail.com>
2018-10-07 11:04:42 +0200

    4.feladat

--- SzamBekeresHF/Program.cs
+++ SzamBekeresHF/Program.cs
@@ -15,20 +15,37 @@ namespace SzamBekeresHF
             List<int> szamLista = new List<int>();
 
             Console.Write("Kérem adja meg, hogy hány számot szeretne megadni : ");
-            lngth = Convert.ToInt32(Console.ReadLine());
-            int[] szamTomb = new int[lngth];
-            Console.WriteLine("Kérem adjon meg " + lngth + "db számot!");
-            do
+            if (int.TryParse(Console.ReadLine(), out lngth))
             {
-                Console.Write((db + 1) + ". szám : ");
-                szam = Convert.ToInt32(Console.ReadLine());
-                szamTomb[db] = szam;
-                szamLista.Add(szam);
-                db++;
-            } while (db < lngth);
+                int[] szamTomb = new int[lngth];
+                Console.WriteLine("Kérem adjon meg " + lngth + "db számot!");
+                do
+                {
+                    Console.Write((db + 1) + ". szám : ");
+                    if (int.TryParse(Console.ReadLine(), out szam))
+                    {
+                        szamTomb[db] = szam;
+                        szamLista.Add(szam);
+                        db++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Legközelebb számot adjon meg!");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                    }
+                } while (db < lngth);
 
-            Console.WriteLine("\n(Tömb) Max : " + MinMaxSzamok.MaxSzam(szamTomb) + "\n(Tömb) Min : " + MinMaxSzamok.MinSzam(szamTomb));
-            Console.WriteLine("\n(Lista) Max : " + szamLista.Max() + "\n(Lista) Min : " + szamLista.Min());
+                Console.WriteLine("\n(Tömb) Max : " + MinMaxSzamok.MaxSzam(szamTomb) + "\n(Tömb) Min : " + MinMaxSzamok.MinSzam(szamTomb));
+                Console.WriteLine("\n(Lista) Max : " + szamLista.Max() + "\n(Lista) Min : " + szamLista.Min());
+            }
+            else
+            {
+                Console.WriteLine("Legközelebb számot adjon meg!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
             Console.ReadKey();
         }
     }
